b222eacf2a4e0e569752f4a9463bd8731798d835
manasgampa <1248@Manas>
2020-07-27 15:03:20 -0400

    new commite

---
+++ batch032020/src/com/antra/ArrayEx.java
@@ -0,0 +1,17 @@
+package com.antra;
+
+public class ArrayEx {
+
+	public static void main(String[] args) {
+		int i[]=new int[3];
+		i[0]=10;
+		i[1]=3;
+		i[2]=67;
+		//i[3]=89;
+		
+		for(int j=0;j<i.length;j++) {
+			
+			System.out.println(i[j]);
+		}
+	}
+}
